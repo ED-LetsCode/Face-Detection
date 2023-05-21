7be0c34b912eb11394646398737dd5835d6c6095
ED-LetsCode <eray.demirel@lets-code.at>
2023-05-21 23:01:11 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,39 @@ import { useEffect, useRef } from "react";
 import "./App.css";
 import * as faceapi from "face-api.js";
 
+const faces = [
+  {
+    name: "Edward Snowden",
+    linkToImage: "EdwardSnowden.jpg",
+  },
+];
+
 export default function App() {
   const videoHeight = 480;
   const videoWidth = 640;
   const canvasRef = useRef();
   const videoRef = useRef();
+  const speechMessage = new SpeechSynthesisUtterance();
+  let detectedFaces = 0;
+  const loadFacesFromImages = async () => {
+    return Promise.all(
+      faces.map(async (face) => {
+        const descriptions = [];
+
+        const img = await faceapi.fetchImage(
+          `src/assets/Images/${face.linkToImage}`
+        );
+
+        const detection = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detection.descriptor);
+
+        return new faceapi.LabeledFaceDescriptors(face.name, descriptions);
+      })
+    );
+  };
 
   const detectFace = async () => {
     canvasRef.current.innerHtml = faceapi.createCanvasFromMedia(
@@ -23,19 +51,38 @@ export default function App() {
     const detections = await faceapi
       .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
+      .withFaceDescriptors()
       .withFaceExpressions()
       .withAgeAndGender();
 
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
     canvasRef.current.getContext("2d").clearRect(0, 0, videoWidth, videoHeight);
 
+    const labeledFaceDescriptors = await loadFacesFromImages();
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+    const matchedFaces = resizedDetections.map((detection) =>
+      faceMatcher.findBestMatch(detection.descriptor)
+    );
+
     resizedDetections.forEach((detection) => {
-      const box = detection.detection.box;
-      const drawBox = new faceapi.draw.DrawBox(box, {
-        label: `Age: ${Math.round(detection.age)} ${detection.gender}`,
-        boxColor: "red",
+      matchedFaces.forEach((name) => {
+        if (!name.toString().includes("unknown")) {
+          if (detectedFaces % 15 === 0) {
+            speechMessage.text = "Hallo " + name.label;
+            speechSynthesis.speak(speechMessage);
+          }
+          detectedFaces++;
+        }
+
+        const box = detection.detection.box;
+        const drawBox = new faceapi.draw.DrawBox(box, {
+          label: `${name} Age: ${Math.round(detection.age)} ${
+            detection.gender
+          }`,
+          boxColor: "red",
+        });
+        drawBox.draw(canvasRef.current);
       });
-      drawBox.draw(canvasRef.current);
     });
 
     faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
@@ -48,7 +95,7 @@ export default function App() {
     }, 500);
   };
 
-  const startVideo = async () => {
+  const startVideoStream = async () => {
     try {
       const videoStream = await navigator.mediaDevices.getUserMedia({
         video: true,
@@ -69,13 +116,14 @@ export default function App() {
       await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
       await faceapi.nets.faceExpressionNet.loadFromUri("/models");
       await faceapi.nets.ageGenderNet.loadFromUri("/models");
+      await faceapi.nets.ssdMobilenetv1.loadFromUri("/models");
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    startVideo();
+    startVideoStream();
     loadModels();
   });
 
